6087241def679c9b466ababd3235755612529638
dominikanykiel <dominika.nykiel.tests@gmail.com>
2022-10-03 15:17:12 +0200

    dashboard changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: "idamud",
+  projectId: "q6w5so",
   viewportHeight: 1000,
   viewportWidth: 1280,
   retries: {
